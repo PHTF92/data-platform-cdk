30137a28b0a22af8e3b67191c0c07831d1691eac
Pedro <phtf92@gmail.com>
2022-02-06 13:35:34 -0300

    Added the Data Migration Service stack

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
 from data_platform.rds.RDS_instance import RDS_instance
+from data_platform.dms.stack import DmsStack
 
 
 app = core.App()
 
 data_lake = DataLakeStack(app)
 rds_instance = RDS_instance(app)
-
+dms = DmsStack(
+    app, common_stack=rds_instance, data_lake_raw_bucket=data_lake.data_lake_raw_bucket
+)
 
 app.synth()
